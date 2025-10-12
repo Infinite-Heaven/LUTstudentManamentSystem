063aaf8b47afb6f3bb86394f53773a5a21d92d0d
Infinite-Heaven <3237982838@qq.com>
2025-10-12 09:29:24 +0800

    初始版本

---
+++ src/main/java/org/lut/studentgrademanagmentsystem/StudentGradeManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.lut.studentgrademanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradeManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentGradeManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lut/studentgrademanagmentsystem/StudentGradeManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lut.studentgrademanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradeManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
