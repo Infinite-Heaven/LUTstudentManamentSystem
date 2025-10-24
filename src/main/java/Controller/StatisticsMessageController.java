3ae833346bd6ba579fb4b7f3baee51bc8da07cda
Infinite-Heaven <3237982838@qq.com>
2025-10-24 11:03:22 +0800

    新增待完善的数据统计功能

---
+++ src/main/java/Controller/StatisticsMessageController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/statisticsMessage")
+@CrossOrigin(origins = "*")
+public class StatisticsMessageController {
+
+
+
+
+}
