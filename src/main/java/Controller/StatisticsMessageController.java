b8cb7bda69dba64183944cca4fd82594fd736b6c
Infinite-Heaven <3237982838@qq.com>
2025-10-25 11:12:00 +0800

    新增了查询平均分与绩点的功能

--- src/main/java/Controller/StatisticsMessageController.java
+++ src/main/java/Controller/StatisticsMessageController.java
@@ -1,15 +1,33 @@
 package Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Service.CourseSelectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/statisticsMessage")
 @CrossOrigin(origins = "*")
 public class StatisticsMessageController {
 
+    @Autowired
+    CourseSelectService courseSelectService;
 
+    //查询学生的所有课程平均分
+    @GetMapping("/studentAverageScore")
+    public Double studentAverageScore(@RequestParam int studentId) {
+return courseSelectService.getAverageScoreForStudent(studentId);
+    }
+    //查询课程的所有学生平均分
+    @GetMapping("/courseAverageScore")
+    public Double courseAverageScore(@RequestParam int courseId) {
+        return courseSelectService.getAverageScoreForCourse(courseId);
+    }
+
+    //查询学生当前选课所有绩点
+    @GetMapping("/studentCredit")
+    public Double studentCredit(@RequestParam int studentId) {
+        return courseSelectService.getCreditScoreForStudent(studentId);
+    }
 
 
 }
