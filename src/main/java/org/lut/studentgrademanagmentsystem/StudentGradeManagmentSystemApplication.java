8033f5dbce49c7e4a2bc8e5b2433e447f73869eb
Infinite-Heaven <3237982838@qq.com>
2025-10-30 10:19:05 +0800

    提交了可以使用的注册界面

--- src/main/java/org/lut/studentgrademanagmentsystem/StudentGradeManagmentSystemApplication.java
+++ src/main/java/org/lut/studentgrademanagmentsystem/StudentGradeManagmentSystemApplication.java
@@ -2,8 +2,18 @@ package org.lut.studentgrademanagmentsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "org.lut.studentgrademanagmentsystem",
+        "Controller",
+        "Service",
+        "Repository",
+        "Entity"
+})
+@EntityScan(basePackages = {"Entity"})
+@EnableJpaRepositories(basePackages = {"Repository"})
 public class StudentGradeManagmentSystemApplication {
 
     public static void main(String[] args) {
